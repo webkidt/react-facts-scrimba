cc0dd95a95c31ae436bb4f9e4f995b956f5f65b3
David Pam <niratpam@gmail.com>
2022-09-22 01:31:44 +0100

    initial commit. project setup.

---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+function Main() {
+  return (<h1>Main section goes here</h1>)
+}
+
+export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return (<h1>Navbar section goes here</h1>)
+}
+
+export default Navbar;
