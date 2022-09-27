1e2782872ff03f59e843b04c55f8f3057763521d
David Pam <niratpam@gmail.com>
2022-09-27 19:40:13 +0100

    styling main section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
+import logo from '../assets/react-icon-small.png';
 function Navbar() {
-  return (<h1>Navbar section goes here</h1>)
+  return (
+    <nav>
+      <img className='nav--icon' src={logo} alt='logo' />
+      <h3 className='nav--logo_text'>ReactFacts</h3>
+      <h4 className='nav--title'>React Course - Project 1</h4>
+    </nav>
+  )
 }
 
 export default Navbar;
