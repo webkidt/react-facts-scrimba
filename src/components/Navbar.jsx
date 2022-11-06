8b8c7221afd86c442c4d53690486394120fa027a
David Pam <niratpam@gmail.com>
2022-11-06 14:17:50 +0100

    inital code base for dark/light theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import logo from '../assets/react-icon-small.png';
-function Navbar() {
+function Navbar(props) {
   return (
-    <nav>
-      <img className='nav--icon' src={logo} alt='logo' />
+    <nav className={props.darkMode ? 'dark' : ''}>
+      <img className='nav--logo_icon' src={logo} />
       <h3 className='nav--logo_text'>ReactFacts</h3>
-      <h4 className='nav--title'>React Course - Project 1</h4>
+
+      <div className='toggler'>
+        <p className='toggler--light'>Light</p>
+        <div className='toggler--slider' onClick={props.toggleDarkMode}>
+          <div className='toggler--slider--circle'></div>
+        </div>
+        <p className='toggler--dark'>Dark</p>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
