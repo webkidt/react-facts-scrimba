b6da077ae6cc38fbfef28c0d778e5b6de76a4c11
David Pam <niratpam@gmail.com>
2022-11-06 19:18:59 +0100

    add darkMode. darkMode state and toggleDarkMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import './App.css';
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode(d => !d);
+  }
   return (
     <div className='container'>
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
